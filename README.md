c722d33307fe659a5901f7bd2094b4603973c9a4
Chal <134571490+clgomez5@users.noreply.github.com>
2023-07-07 19:23:10 -0700

    added README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# challenge-3
+# Javascript Calculator
+
+## Description
+
+The Javascript calculator will ask you for a series of prompts on whether or not you want lowercase, uppercase, numbers, or special characters. In addition to this, it will inform you if you have the required password length. It helps to create a strong, reliable, and secure password for the user to use.
+
+## Usage
+
+Press the generate password button. Once this is pressed, you will be given a series of prompts regarding password criteria.
+![Alt text](<password generator.png>)
+
--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var mygeneratedPassword = {
+  num: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  lCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  uCase: "ABCDEFGHIJKLMNOPQRSTUV".split(""),
+  special: "!@#*$".split(""),
+  passLength: 8
+}
+
+console.log(mygeneratedPassword.length);
+
+function generatePassword () {
+  var mygeneratedPassword = "";
+
+    window.prompt("How many characters do you want your password?");
+
+    window.prompt("Do you want lowercase letters?");
+    if (Math.floor(Math.random() * lCase.length));
+
+    window.prompt("Do you want a uppercase character?");
+
+    window.prompt("Do you want a number?");
+    if (Math.floor(Math.random() * 10));
+
+    window.prompt("Do you want special characters?");
+
+  return mygeneratedPassword;    
+    }
+
+generatePassword ();
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,51 +43,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-//console.log(num.length);
-//console.log(lCase.length);
-//console.log(uCase.length);
-//console.log(special.length);
-
-var mygeneratedPassword = {
-  num: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  lCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  uCase: "ABCDEFGHIJKLMNOPQRSTUV".split(""),
-  special: "!@#*$".split(""),
-}
-
-function generatePassword () {
-  var mygeneratedPassword = "";
-  window.prompt("How many characters do you want your password?");
-  
-    window.alert("Do you want lowercase letters?");
-
-    window.alert("Do you want a uppercase character?");
-
-    window.alert("Do you want a number?");
-
-    window.alert("Do you want special characters?");
-
-  return mygeneratedPassword;    
-    }
-
-  
-
- 
-  
-
-
-
-
-console.log(mygeneratedPassword)
-
-function mygeneratedPassword (length = 8) {
-  var password = " ";
-  var options = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%*()?ABCDEFGHIJKLMNOPQRSTUVWZYZ";
-  var passLength = 8
-}
-
-var expression1 = (passLength >= 8);
+// function mygeneratedPassword (length = 8) {
+//   var password = " ";
+//   var options = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%*()?ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+//   var passLength = 8
+// }
 
 
 
@@ -65,4 +55,4 @@ var expression1 = (passLength >= 8);
 
 
 
-generatePassword ();
+
